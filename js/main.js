b058a11adb1bf559e0b8928ca67d5036e788f6ee
sergio <gsricco@yandex.ru>
2024-07-13 13:35:23 +0300

    fix(layout): added styles

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const modal = document.getElementById("myModal");
 const btn = document.getElementById("openModalBtn");
 const span = document.getElementsByClassName("close")[0];
 
+
 btn.onclick = function() {
     modal.style.display = "flex";
 }
