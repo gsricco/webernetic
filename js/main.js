82b707b7dd3541d88103f47caa7394eb9b206b26
sergio <gsricco@yandex.ru>
2024-07-13 13:47:18 +0300

    feat(logic): added login processing logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const modal = document.getElementById("myModal");
 const btn = document.getElementById("openModalBtn");
 const span = document.getElementsByClassName("close")[0];
+const form = document.getElementById("loginForm");
 
 
 btn.onclick = function() {
@@ -16,3 +17,39 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const data = {
+        email: formData.get("email"),
+        password: formData.get("password"),
+        rememberMe: formData.get("remember-me") ? true : false,
+    };
+
+
+    //!!!!Измените на правильный url!!!!
+    fetch("/your-domain/endpoint-yourAPI/example", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log("Success:", data);
+            modal.style.display = "none";
+            alert("Сообщение отправлено. Ожидайте входа");
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+            alert("Произошла ошибка при входе. Пожалуйста, попробуйте снова.");
+        });
+});
